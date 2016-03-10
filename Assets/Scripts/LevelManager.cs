8b4c3b4e0c14ff19555a844c75a38bf1612ee137
David Ouyang <douyang@gmail.com>
2016-03-09 19:40:45 -0800

    First commit
    Added splash screen
    Added first start menu

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadDelay;
+
+
+	void Start () {
+		AutoLoadNextLevel ();
+	}
+
+	public void AutoLoadNextLevel () {
+		Invoke ("LoadNextLevel", autoLoadDelay);
+	}
+
+	public void LoadLevel(string name) {
+		Debug.Log ("level load requested for " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void LoadNextLevel ()
+	{
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+
+	public void QuitRequest() {
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+}
