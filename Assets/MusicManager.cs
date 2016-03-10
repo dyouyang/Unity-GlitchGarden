69dcd0d260264bdb419aefbbdebbb269d7e81911
David Ouyang <David Ouyang>
2016-03-09 22:14:32 -0800

    Add a persistent music manager with an array of audio clips to play
    when a specific level is loaded.

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip [] clipForLevel;
+
+	private AudioSource audioSource;
+
+	void Awake () {
+		DontDestroyOnLoad (gameObject);
+	}
+
+	void OnLevelWasLoaded (int level) {
+		if (clipForLevel [level] != null) {
+			audioSource.clip = clipForLevel[level];
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+}
