1e0440a53d7cdfa7dc5e3a917ecd63c23b3248c3
David Ouyang <David Ouyang>
2016-03-10 21:16:09 -0800

    Add a fade in effect to start scene.

---
+++ Assets/PanelFader.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PanelFader : MonoBehaviour {
+
+    public float duration;
+
+    private Image image;
+    private Color startColor = Color.black;
+
+	// Use this for initialization
+	void Start () {
+        image = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Time.timeSinceLevelLoad < duration) {
+
+            // My solution
+            float alpha = 1 - Time.timeSinceLevelLoad / duration;
+            image.GetComponent<CanvasRenderer>().SetAlpha(alpha);
+
+
+            // Instructor solution
+            //float alpha = Time.deltaTime / duration;
+            //startColor.a -= alpha;
+            //image.color = startColor;
+        } else {
+            gameObject.SetActive(false);
+        }
+	}
+}
