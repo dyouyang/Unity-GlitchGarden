4db7e0bdb5799fd17a825a5e70911d8f42cffce6
David Ouyang <David Ouyang>
2016-03-20 13:04:41 -0700

    Add an Attacker component that allows attackers to walk
    Set a public variable for walk speed.  Temporarily mute animation
    transition so we can get walk right.

---
+++ Assets/Attacker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacker : MonoBehaviour {
+
+    [Range (-2f, 2f)]
+    public float walkSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 movementVector = Vector3.left * walkSpeed * Time.deltaTime;
+        transform.Translate(movementVector);
+        //this.transform.position += movementVector;
+	}
+}
