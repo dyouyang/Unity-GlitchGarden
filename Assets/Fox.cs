507e4bc244934bffaa6b1201c9d9aec14e04f2c2
David Ouyang <David Ouyang>
2016-03-26 13:14:50 -0700

    Add collision behavior to Fox
    Fox jumps if it hits anything that has a Stone component, and attacks
    otherwise.

---
+++ Assets/Fox.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Attacker))]
+public class Fox : MonoBehaviour {
+
+    private Attacker attacker;
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        attacker = GetComponent<Attacker>();
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        GameObject collidedWith = collider.gameObject;
+
+        if (collidedWith.GetComponent<Defender>() != null) {
+
+            if (collidedWith.GetComponent<Stone>() != null) {
+                animator.SetTrigger("doJumpTrigger");
+            } else {
+                attacker.Attack(collidedWith);
+            }
+        }
+    }
+}
