7d4edd34a0fd8984f80a1c30763e72c382319f62
David Ouyang <David Ouyang>
2016-04-10 00:07:18 -0700

    Add a shooter component, spawn projectiles on animations
    Shooter component spawns a projectile at a defender's Gun position.

---
+++ Assets/Shooter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+
+    public GameObject gun;
+
+    public GameObject axe;
+    public GameObject projectilesParent;
+
+    private void FireGun () {
+        GameObject axeProjectile = Instantiate(axe, gun.transform.position, Quaternion.identity) as GameObject;
+        axeProjectile.transform.parent = projectilesParent.transform;
+    }
+}
