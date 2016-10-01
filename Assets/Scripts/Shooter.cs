9e32f15ad5adaccd215ee86a20e2d75400428290
David Ouyang <David Ouyang>
2016-10-01 01:07:32 -0700

    Lazily spawn a Projectiles parent to keep projectiles from cluttering.

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -6,8 +6,15 @@ public class Shooter : MonoBehaviour {
     public GameObject gun;
 
     public GameObject axe;
-    public GameObject projectilesParent;
+    private GameObject projectilesParent;
 
+    private const string PROJECTILES_NAME = "Projectiles";
+    void Start () {
+        projectilesParent = GameObject.Find(PROJECTILES_NAME);
+        if (projectilesParent == null) {
+            projectilesParent = new GameObject(PROJECTILES_NAME);
+        }
+    }
     private void FireGun () {
         GameObject axeProjectile = Instantiate(axe, gun.transform.position, Quaternion.identity) as GameObject;
         axeProjectile.transform.parent = projectilesParent.transform;
