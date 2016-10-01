12d84e3fef3d8e323824e3c367b13e2765a4d94c
David Ouyang <David Ouyang>
2016-10-01 00:09:51 -0700

    Add panel with buttons to select defenders.
    Add simple click handling.

---
+++ Assets/Button.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown () {
+        print(name + " clicked");
+        SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
+        renderer.color = Color.black;
+    }
+}
