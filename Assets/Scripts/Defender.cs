84d9be36de608cb30ddc320db12aabbb4717b472
David Ouyang <David Ouyang>
2016-10-01 23:15:21 -0700

    Add currency (stars), display and incrementing when spawn.

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class Defender : MonoBehaviour {
 
+    private StarKeeper starKeeper;
+
 	// Use this for initialization
 	void Start () {
-	
+        starKeeper = FindObjectOfType<StarKeeper>();
 	}
 	
 	// Update is called once per frame
@@ -15,5 +17,9 @@ public class Defender : MonoBehaviour {
 
     void OnTriggerEnter2D() {
         Debug.Log(name + " OnTriggerEnter2D");
+    }
+
+    public void AddStars(int numStarsToAdd) {
+        starKeeper.AddStars(numStarsToAdd);
     }
 }
---
+++ Assets/Scripts/StarKeeper.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof(Text))]
+public class StarKeeper : MonoBehaviour {
+
+    private int numStars;
+
+    private Text text;
+
+	// Use this for initialization
+	void Start () {
+        numStars = 0;
+        text = GetComponent<Text>();
+        UpdateDisplay();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void AddStars(int numStarsToAdd) {
+        numStars += numStarsToAdd;
+        UpdateDisplay();
+    }
+
+    public void UseStars(int numStarsToUse) {
+        numStars -= numStarsToUse;
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay() {
+        text.text = numStars.ToString();
+    }
+}
