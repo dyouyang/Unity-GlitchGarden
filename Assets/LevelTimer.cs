790e6ac95ef1c91b6a3340bad0b02d9cdd25e123
David Ouyang <David Ouyang>
2016-10-02 18:44:12 -0700

    Add win condition to level 01
    Win when the timer reaches the end, play sound and then load next level.

---
+++ Assets/LevelTimer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelTimer : MonoBehaviour {
+
+    public float levelTimer = 60f;
+    public LevelManager levelManager;
+    public Text winText;
+
+    private bool endedLevel = false;
+	
+	// Update is called once per frame
+	void Update () {
+        float timePercentage = Time.timeSinceLevelLoad / levelTimer;
+        GetComponent<Slider>().value = timePercentage;
+
+        if (Time.timeSinceLevelLoad >= levelTimer && !endedLevel) {
+            WinLevel();
+        }
+	}
+
+    private void WinLevel() {
+        print("Level won!");
+        endedLevel = true;
+        winText.gameObject.SetActive(true);
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+        Invoke("LoadNextLevel", audioSource.clip.length);
+    }
+
+    private void LoadNextLevel() {
+        levelManager.LoadNextLevel();
+    }
+}
